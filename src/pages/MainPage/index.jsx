e044ff517a532aee69238cafcfb1cb004a2e0cb6
nagiltae <nagt1997@gmail.com>
2024-07-20 22:44:23 +0900

    video data

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { getVideoInfo } from '../../helpers/fetchingDAta'
 
 const MainPage = () => {
+
+  const [mainVideos, setMainVideos] = useState([])
+
+  const getMainVideos = useCallback( async()=>{
+    try{
+      const response = await axios.get(`/search?part=snippet&maxResults=10&q=beautiful%20place`)
+      let videoArray = await  response.data.items      
+      //console.log(videoArray)
+
+      videoArray = await getVideoInfo(videoArray)
+      setMainVideos(videoArray)
+
+    }catch(e){
+      console.log(e)
+    }
+
+  }, [])
+
+  useEffect(( )=> {
+    getMainVideos();
+  }, [getMainVideos])
+
   return (
     <div>
       MainPage
